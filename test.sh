c3da8ea8e8de807686c7a872b69846cc4ccd17f7
mmmner <mm_mner@163.com>
2024-04-26 19:35:56 +0800

    fixed

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 python test.py \
     --bart_name facebook/bart-base \
     --model_weight ./saved_model/best_model \
-    --datapath  data/Twitter10000_v2.0/txts/total_txt \
+    --datapath  data/Twitter10000_v2.0/txt \
     --image_feature_path data/Twitter10000_IETrans \
     --image_annotation_path data/Twitter10000_v2.0/xml \
     --box_num 18 \
